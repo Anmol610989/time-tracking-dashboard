b246b35352a9a5d64d1b93e139f9cb6695e5030a
Anmol610989 <67320791+Anmol610989@users.noreply.github.com>
2025-03-17 13:18:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,152 +1,68 @@
+const display = document.querySelector(".display-grid");
+const buttons = document.querySelectorAll(".btn");
 
 
-async function populate() {
-    const requestURL =
-      "./data.json";
-    const request = new Request(requestURL);
+const populate = async () => {
+  const url = "./data.json";
 
-    const response = await fetch(request);
-    const data = await response.json()
+  const resp = await fetch(url);
+  const data = await resp.json();
 
-      updateWeekly(data);
-  }
-
-
-    function updateDaily(obj){
-
-      let mySection = document.querySelectorAll('.time');
   
-      for(let i = 0; i< mySection.length; i++){
-  
-        let dailyH1 = document.createElement('span');
-        let dailyPara = document.createElement('p');
-          for(let j = i; j<= i; j++){
-            dailyH1.textContent = `${obj[i].timeframes.daily.current}hrs`
-            dailyPara.textContent = `Yesterday - ${obj[i].timeframes.daily.previous}hrs`
-        }
-  
-        mySection[i].appendChild(dailyH1);
-        mySection[i].appendChild(dailyPara);
-      }
-    }
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const timeframe = button.innerText.toLowerCase();
+      updateData(data, timeframe);
 
-
-    function updateWeekly(obj){
-
-      let mySection = document.querySelectorAll('.time');
-  
-      for(let i = 0; i< mySection.length; i++){
-  
-        let weekh1 = document.createElement('span');
-        let weekPara = document.createElement('p');
-          for(let j = i; j<= i; j++){
-            weekh1.textContent = `${obj[i].timeframes.weekly.current}hrs`
-            weekPara.textContent = `previous - ${obj[i].timeframes.weekly.previous}hrs`
-        }
-  
-        mySection[i].appendChild(weekh1);
-        mySection[i].appendChild(weekPara);
-      }
-  
-    }
-
-    function updateMonthly(obj){
-
-      let mySection = document.querySelectorAll('.time');
-  
-      for(let i = 0; i< mySection.length; i++){
-  
-        let monthlyh1 = document.createElement('span');
-        let monthlyPara = document.createElement('p');
-          for(let j = i; j<= i; j++){
-            monthlyh1.textContent = `${obj[i].timeframes.monthly.current}hrs`
-            monthlyPara.textContent = `previous - ${obj[i].timeframes.monthly.previous}hrs`
-        }
-  
-        mySection[i].appendChild(monthlyh1);
-        mySection[i].appendChild(monthlyPara);
-      }
-  
-    }
-  
-
-  populate()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // let nodelist = document.querySelectorAll('.time')
-    // let mySection = Array.from(nodelist)
-    // console.log(mySection)
-
-
-    //   let daily = [];
-    //   let previous = [];
-
-    //     for(let i = 0; i< obj.length; i++){
-
-    //       daily.push(obj[i].timeframes.daily.current)
-
-    //     }
-
-    //     for(let i = 0; i< obj.length; i++){
-
-    //       previous.push(obj[i].timeframes.daily.previous)
-
-    //     }
-    //     console.log(daily)
-    //     console.log(previous)
-
-    //     for(let i = 0; i<mySection.length; i++){
-    //         let myH1 = document.createElement("h1")
-    //         let myPara = document.createElement('p');
-
-    //         myH1.textContent = daily[i];
-    //         myPara.textContent = previous[i];
-
-
-    //         mySection[i].appendChild('myH1')
-    //         mySection[i].appendChild('myPara')
-
-    //     }
-    //     console.log(mySection)
-
-
-
-
-    // myH1.textContent = `${obj[i].timeframes.daily.current}Hrs`
-
-    // item.appendChild(myH1)
-           
-    // })
-
+      // enable buttons that are not clicked 
+      buttons.forEach((btn) => (btn.disabled = false));
+      // disable the clicked button
+      button.disabled = true;
+    });
    
-
-    // timeframes
-
     
-    // console.log(mySection)
+    })
+ 
 
+// initialize the data on loading the page 
+updateData(data, "daily");
+ buttons.forEach((button) => {
+   if (button.innerText === "daily") {button.disabled = true};
+ });
 
+}; 
+
+function updateData(data, timeframe) {
+  display.innerHTML = ""; // Clear the display
+
+  data.forEach((element) => {
+    const { title, timeframes } = element;
     
-   
+    const current = timeframes[timeframe].current;
+    const previous = timeframes[timeframe].previous;
 
+    const list = document.createElement("div");
+    list.innerHTML = `
+      <div class="grid-item br ${title}-info">
+        <div class="icon"><div class="${title}"></div></div>
+        <div class="${title} container">
+          <div class="heading">
+            <h2>${title}</h2>
+            <div>
+              <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/>
+              </svg>
+            </div>
+          </div>
+          <div class="flex">
+            <div class="time">${current}hrs</div>
+            <div class="prev">Previous - ${previous}hrs</div>
+          </div>
+        </div>
+      </div>
+    `;
+    display.appendChild(list);
+  });
+}
 
-  
-
-
-
-  
-  
+populate();
